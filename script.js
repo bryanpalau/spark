6eacb6fa1ad2db031b36d2474f41ce157e471c5d
bryanpalau <bryanpalau@gmail.com>
2025-01-15 22:33:20 +0800

    new js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(window).bind('load', function () {
     const random = function (min, max) {
         max = max + 1;
         return Math.floor(Math.random() * (max - min) + min);
-    }
+    };
     var app = {
         init: function () {
             this.cacheDOM();
@@ -13,9 +13,6 @@ $(window).bind('load', function () {
         },
         cacheDOM: function () {
             this.container = $('#container');
-
-            // No images in this project, comment out the unnecessary parts
-            // this.images = $('img');
             this.mouseX = null;
             this.mouseY = null;
         },
@@ -31,7 +28,7 @@ $(window).bind('load', function () {
             app.mouseX = e.clientX;
             app.mouseY = e.clientY;
         }
-    }
+    };
 
     app.init();
 
@@ -48,14 +45,13 @@ $(window).bind('load', function () {
     c2.width = $('#c2').outerWidth();
     c2.height = $('#c2').outerHeight();
 
-    //INITIAL CANVAS DRAW
+    // INITIAL CANVAS DRAW
     cx.fillStyle = 'rgba(0,0,0,1)';
     cx.fillRect(0, 0, c.width, c.height);
     c2x.fillStyle = 'rgba(0,0,0,1)';
     c2x.fillRect(0, 0, c2.width, c2.height);
 
     function particleFactory(thisCanvas, thisContext, thisParticleName, thisCanvasFunction) {
-
         var particleIndex = 0,
             particles = {},
             particleNum = 2;
@@ -71,7 +67,7 @@ $(window).bind('load', function () {
             particleIndex++;
             particles[particleIndex] = this;
             this.id = particleIndex;
-        }
+        };
 
         thisParticleName.prototype.draw = function () {
             thisContext.fillStyle = this.color;
@@ -85,7 +81,7 @@ $(window).bind('load', function () {
             if (this.x > thisCanvas.width || this.x < 0 || this.y > thisCanvas.height || this.y < 0) {
                 delete particles[this.id];
             }
-        }
+        };
 
         thisCanvasFunction = function () {
             thisContext.fillStyle = 'rgba(0,0,0,0.1)';
@@ -100,10 +96,32 @@ $(window).bind('load', function () {
             }
 
             raf(thisCanvasFunction);
-        }
+        };
         raf(thisCanvasFunction);
     }
 
     particleFactory(c, cx, "Particle", canvas);
     particleFactory(c2, c2x, "Particle2", canvas2);
+
+    TweenMax.set(c2Container, {
+        transformOrigin: 'center bottom',
+        scaleY: -1,
+        opacity: 1
+    });
+
+    TweenMax.set(c2, {
+        filter: 'blur(10px)'
+    });
+
+    $(window).resize(function initial() {
+        window.addEventListener('mousemove', app.cursorEvents, false);
+
+        c.width = $('#c').outerWidth();
+        c.height = $('#c').outerHeight();
+
+        c2.width = $('#c2').outerWidth();
+        c2.height = $('#c2').outerHeight();
+
+        return initial;
+    }());
 });
