bc445c9f6e29547fd4b1ca4b3f6dd4c96d47c4e8
bryanpalau <bryanpalau@gmail.com>
2025-01-16 11:16:42 +0800

    o

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ $(window).bind('load', function () {
         max = max + 1;
         return Math.floor(Math.random() * (max - min) + min);
     };
-
     var app = {
         init: function () {
             this.cacheDOM();
@@ -33,7 +32,9 @@ $(window).bind('load', function () {
 
     app.init();
 
-    var c = document.getElementById('c'),
+    var cContainer = $('#c-container'),
+        c = document.getElementById('c'),
+        c2Container = $('#c2-container'),
         c2 = document.getElementById('c2'),
         cx = c.getContext('2d'),
         c2x = c2.getContext('2d');
@@ -50,30 +51,25 @@ $(window).bind('load', function () {
     c2x.fillStyle = 'rgba(0,0,0,1)';
     c2x.fillRect(0, 0, c2.width, c2.height);
 
-    function particleFactory(thisCanvas, thisContext) {
+    function particleFactory(thisCanvas, thisContext, thisParticleName, thisCanvasFunction) {
         var particleIndex = 0,
             particles = {},
-            particleNum = 10;
+            particleNum = 2;
 
-        function Particle() {
+        thisParticleName = function () {
             this.r = 8;
-            this.rIncrement = -0.1;
             this.x = thisCanvas.width / 2;
             this.y = thisCanvas.height / 2;
 
-            this.vx = random(-3, 3);
-            this.vy = random(-3, 3);
-            this.opacity = 1;
-            this.hue = random(30, 60);
-            this.lightness = random(50, 70);
-            this.color = `hsla(${this.hue},100%,${this.lightness}%,${this.opacity})`;
-
+            this.vx = random(-5, 5);
+            this.vy = random(-5, 5);
+            this.color = `hsla(${random(30, 60)},100%,50%,1)`;
             particleIndex++;
             particles[particleIndex] = this;
             this.id = particleIndex;
-        }
+        };
 
-        Particle.prototype.draw = function () {
+        thisParticleName.prototype.draw = function () {
             thisContext.fillStyle = this.color;
             thisContext.beginPath();
             thisContext.arc(this.x, this.y, this.r, 0, Math.PI * 2);
@@ -81,17 +77,13 @@ $(window).bind('load', function () {
 
             this.x += this.vx;
             this.y += this.vy;
-            this.r += this.rIncrement;
-            this.opacity -= 0.02;
 
-            if (this.opacity <= 0 || this.r <= 0) {
+            if (this.x > thisCanvas.width || this.x < 0 || this.y > thisCanvas.height || this.y < 0) {
                 delete particles[this.id];
             }
-
-            this.color = `hsla(${this.hue},100%,${this.lightness}%,${this.opacity})`;
         };
 
-        function renderParticles() {
+        thisCanvasFunction = function () {
             thisContext.fillStyle = 'rgba(0,0,0,0.1)';
             thisContext.fillRect(0, 0, thisCanvas.width, thisCanvas.height);
 
@@ -100,19 +92,18 @@ $(window).bind('load', function () {
             }
 
             if (Object.keys(particles).length < particleNum) {
-                new Particle();
+                new thisParticleName();
             }
 
-            raf(renderParticles);
-        }
-
-        raf(renderParticles);
+            raf(thisCanvasFunction);
+        };
+        raf(thisCanvasFunction);
     }
 
-    particleFactory(c, cx);
-    particleFactory(c2, c2x);
+    particleFactory(c, cx, "Particle", canvas);
+    particleFactory(c2, c2x, "Particle2", canvas2);
 
-    TweenMax.set('#c2-container', {
+    TweenMax.set(c2Container, {
         transformOrigin: 'center bottom',
         scaleY: -1,
         opacity: 1
@@ -122,10 +113,15 @@ $(window).bind('load', function () {
         filter: 'blur(10px)'
     });
 
-    $(window).resize(function () {
+    $(window).resize(function initial() {
+        window.addEventListener('mousemove', app.cursorEvents, false);
+
         c.width = $('#c').outerWidth();
         c.height = $('#c').outerHeight();
+
         c2.width = $('#c2').outerWidth();
         c2.height = $('#c2').outerHeight();
-    });
+
+        return initial;
+    }());
 });
